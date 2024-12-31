6886c220a5505aacf60fc4384dd813d335615ad7
Bhasavasaranya-thirumoorthy <bhasavasaranya08@gmail.com>
2024-12-31 15:29:55 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -12,6 +12,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); 
@@ -20,6 +22,14 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
 app.use("/api/users",userRoutes);
+
+
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"frontend", "dist", "index.html"))
+})
 
 
 
